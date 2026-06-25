5ebdabdfd7c4f9ba64d17dfa936aae98cec80ae3
AryanBehl <aryanbehla914@gmail.com>
2026-06-25 13:46:23 +0530

    Added file

--- week-3/day-3/src/main.jsx
+++ week-3/day-3/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ThemeContextProvider from "./Context/ThemeContext.jsx";
+import AuthContextProvider from "./Context/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <ThemeContextProvider>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </ThemeContextProvider>
+);
