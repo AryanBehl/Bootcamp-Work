0a8714843aa2ed6b036b0ccd767807d622ee107b
AryanBehl <aryanbehla914@gmail.com>
2026-06-29 15:25:47 +0530

    1st Day of Back-End

---
+++ week-4-Back-End/day-1/server.js
@@ -0,0 +1,12 @@
+// const http = require('http')
+import http from 'http'
+
+const port = 3000
+
+const server = http.createServer((req,res) => {
+
+})
+
+server.listen(port , ()=>{
+    console.log("Server is listen in port:", port);
+})
