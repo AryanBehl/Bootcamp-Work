19ed92ea3728b963c4124f379c67c0429ec65e55
AryanBehl <aryanbehla914@gmail.com>
2026-07-14 09:49:55 +0530

    Added all file

---
+++ week-4-Back-End/day-3/database/data.js
@@ -0,0 +1,19 @@
+const employee = [
+    {
+        name : "Aryan",
+        email : "Aryan@gmail.com",
+        empId : 123
+    },
+    {
+        name : "Naveen",
+        email : "Naveen@gmail.com",
+        empId : 234
+    },
+    {
+        name : "Yash",
+        email : "Yash@gmail.com",
+        empId : 456
+    }
+]
+
+export default employee
